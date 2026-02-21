faa64c96d88a66e6765071cec2381d7b7319ca16
Ryce <reqin0@mail.ru>
2026-02-21 13:39:04 +0300

    v 2.0.0

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        Double firstNumber = scanner.nextDouble();
+
+        System.out.println("Введите операцию (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        System.out.println("Введите второе число: ");
+        Double secondNumber = scanner.nextDouble();
+
+
+        double resualt = 0;
+
+        switch (operation) {
+            case '+' -> resualt = firstNumber + secondNumber;
+            case '-' -> resualt = firstNumber - secondNumber;
+            case '*' -> resualt = firstNumber * secondNumber;
+            case '/' -> resualt = firstNumber / secondNumber;
+            default -> {
+                System.out.println("Ошибка: неверная операция или деление на ноль!");
+                return;
+            }
+
+        };
+        System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + resualt);
+    }
 }
+
+
+
