3d0847098b319531f40dd59bab609a01338a5b76
Ryce <reqin0@mail.ru>
2026-02-25 11:16:23 +0300

    v 3.0.5

--- src/Main.java
+++ src/Main.java
@@ -8,58 +8,76 @@ public class Main {
         double secondNumber;
         char operation;
         double result;
+        String exit;
 
         // ### Сканер ###
         Scanner scanner = new Scanner(System.in);
-
-        // ### Работа с первым числом ###
-        System.out.println("Введите первое число: ");
         while (true) {
+            // ### Работа с первым числом ###
+            System.out.println("Введите первое число: ");
+            while (true) {
 
-            try {
-                firstNumber = scanner.nextDouble();
-                break;
-            } catch (Exception e) {
+                try {
+                    firstNumber = scanner.nextDouble();
+                    break;
+                } catch (Exception e) {
+                    scanner.nextLine();
+                    System.out.println("Некорректное число. Введите еще раз: ");
+                }
+
+            }
+            // ### Работа с оператором ###
+            System.out.println("Введите операцию (+, -, *, /): ");
+            while (true) {
+                operation = scanner.next().charAt(0);
+                if (operation == '+' || operation == '-' ||
+                        operation == '*' || operation == '/') {
+                    break;
+                } else {
+                    System.out.println("Некорректный оператор. Введите еще раз: ");
+                }
+
+            }
+            // ### Работа со вторым числом ###
+            System.out.println("Введите второе число: ");
+            while (true) {
+                try {
+                    secondNumber = scanner.nextDouble();
+                    //### Деление на ноль ###
+                    if (operation == '/' && secondNumber == 0) {
+                        System.out.println("Нельзя делить на ноль. Введите другое число: ");
+                        continue;
+                    }
+                    break;
+                } catch (Exception e) {
+                    scanner.nextLine();
+                    System.out.println("Некорректное число. Введите еще раз: ");
+                }
+
+            }
+            // ### Операции ###
+            switch (operation) {
+                case '+' -> result = firstNumber + secondNumber;
+                case '-' -> result = firstNumber - secondNumber;
+                case '*' -> result = firstNumber * secondNumber;
+                case '/' -> result = firstNumber / secondNumber;
+                default -> throw new IllegalStateException("Ошибка оператора");
+            }
+
+            // ### Вывод ответа ###
+            System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
+
+            // ### Выход и подтверждение ###
+            while (true) {
+                System.out.println("Продолжить? (да/нет): ");
                 scanner.nextLine();
-                System.out.println("Некорректное число. Введите еще раз: ");
-            }
-
-        }
-        // ### Работа с оператором ###
-        System.out.println("Введите операцию (+, -, *, /): ");
-        while (true) {
-            operation = scanner.next().charAt(0);
-            if (operation == '+' || operation == '-' ||
-                    operation == '*' || operation == '/') {
-                break;
-            } else {
-                System.out.println("Некорректный оператор. Введите еще раз: ");
-            }
-
-        }
-        // ### Работа со вторым числом ###
-        System.out.println("Введите второе число: ");
-        while (true) {
-            try {
-                secondNumber = scanner.nextDouble();
-                break;
-            } catch (Exception e) {
-                scanner.nextLine();
-                System.out.println("Некорректное число. Введите еще раз: ");
+                exit = scanner.nextLine();
+                if (exit.equals("да")) {
+                    break;
+                } else if (exit.equals("нет")) {
+                    return;
+                }
             }
         }
-        // ### Операции ###
-        switch (operation) {
-            case '+' -> result = firstNumber + secondNumber;
-            case '-' -> result = firstNumber - secondNumber;
-            case '*' -> result = firstNumber * secondNumber;
-            case '/' -> result = firstNumber / secondNumber;
-            default -> throw new IllegalStateException("Ошибка оператора");
-        }
-        // ### Вывод ответа ###
-        System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
-
-        // ### Выход и подтверждение ###
-
     }
 }
