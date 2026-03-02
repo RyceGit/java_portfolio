58201100df2844e08d1d93fc8cce37ed93bc0d5b
Ryce <reqin0@mail.ru>
2026-03-02 11:41:52 +0300

    v 4.2.8

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public static double calculate ( char operation, double firstNumber, double secondNumber){
+    public  double calculate ( char operation, double firstNumber, double secondNumber){
         double result;
         switch (operation) {
             case '+' -> result = firstNumber + secondNumber;
