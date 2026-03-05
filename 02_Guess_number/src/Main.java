4d8804e354b1552b926fc4fda949cc4055ca7b93
Ryce <reqin0@mail.ru>
2026-03-05 17:47:29 +0300

    v 20.0.0 Угадай число

--- 02_Guess_number/src/Main.java
+++ 02_Guess_number/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("фыва");
+        Random rand = new Random();
+        int random = rand.nextInt(51);
+        System.out.println("Я загадал число от 1 до 50. Попробуй угадать!");
+        System.out.println("Твоя догадка: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (true){
+            i++;
+            int myNumber = scanner.nextInt();
+
+            if (myNumber < random) {
+                System.out.println("Слишком мало!");
+            }
+            else if(myNumber > random) {
+                System.out.println("Слишком много!");
+            }
+            else {
+                System.out.println("Угадал! Ты справился с " + i + " попытки.");
+                return;
+            }
+        }
     }
 }
